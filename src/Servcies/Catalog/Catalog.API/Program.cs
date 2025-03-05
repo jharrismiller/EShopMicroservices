f7e1c7b1753418be17165c2268030b5069d03ea8
jharrismiller <36432119+jharrismiller@users.noreply.github.com>
2025-03-05 13:45:35 -0500

    https http post numbers

--- src/Servcies/Catalog/Catalog.API/Program.cs
+++ src/Servcies/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
+
+//Add Services to the container
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+//Configure the HTTP request pipeline
+
 
 app.Run();
