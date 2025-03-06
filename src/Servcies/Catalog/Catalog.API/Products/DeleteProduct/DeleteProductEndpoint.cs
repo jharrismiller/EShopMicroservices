1dc8f084f288fbe02008746da89b5455c4409868
jharrismiller <36432119+jharrismiller@users.noreply.github.com>
2025-03-06 00:02:34 -0500

    clean up

--- src/Servcies/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
+++ src/Servcies/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -1,29 +1,24 @@
-﻿
-using Catalog.API.Products.GetProductByCategory;
+﻿namespace Catalog.API.Products.DeleteProduct;
 
-namespace Catalog.API.Products.DeleteProduct
+public record DeleteProductRequest(Guid Id);
+
+public record DeleteProductResponse(bool IsSuccess);
+
+public class DeleteProductEndpoint : ICarterModule
 {
-
-    public record DeleteProductRequest(Guid Id);
-
-    public record DeleteProductResponse(bool IsSuccess);
-
-    public class DeleteProductEndpoint : ICarterModule
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
-        public void AddRoutes(IEndpointRouteBuilder app)
+        app.MapDelete("/products/{id}", async (Guid id, ISender sender) =>
         {
-            app.MapDelete("/products/{id}", async (Guid id, ISender sender) =>
-            {
 
-                var result = await sender.Send(new DeleteProductCommand(id));
+            var result = await sender.Send(new DeleteProductCommand(id));
 
-                return Results.Ok(result.Adapt<DeleteProductResponse>());
-            })
-            .WithName("DeleteProduct")
-            .Produces<DeleteProductResponse>(StatusCodes.Status200OK)
-            .ProducesProblem(StatusCodes.Status400BadRequest)
-            .WithSummary("Delete Product")
-            .WithDescription("Delete Product Desc");
-        }
+            return Results.Ok(result.Adapt<DeleteProductResponse>());
+        })
+        .WithName("DeleteProduct")
+        .Produces<DeleteProductResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Delete Product")
+        .WithDescription("Delete Product Desc");
     }
 }
--- src/Servcies/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
+++ src/Servcies/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -1,23 +1,21 @@
 ﻿
-namespace Catalog.API.Products.DeleteProduct
+namespace Catalog.API.Products.DeleteProduct;
+
+public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductCommandResult>;
+
+public record DeleteProductCommandResult(bool IsSuccess);
+
+public class DeleteProductCommandHandler(IDocumentSession documentSession, ILogger<DeleteProductCommandHandler> logger) : ICommandHandler<DeleteProductCommand, DeleteProductCommandResult>
 {
-
-    public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductCommandResult>;
-
-    public record DeleteProductCommandResult(bool IsSuccess);
-
-    public class DeleteProductCommandHandler(IDocumentSession documentSession, ILogger<DeleteProductCommandHandler> logger) : ICommandHandler<DeleteProductCommand, DeleteProductCommandResult>
+    public async Task<DeleteProductCommandResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
     {
-        public async Task<DeleteProductCommandResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
-        {
 
-            logger.LogInformation("DeleteProductCommandHandler.Handle called with {@Command}", command);
+        logger.LogInformation("DeleteProductCommandHandler.Handle called with {@Command}", command);
 
-            documentSession.Delete<Product>(command.Id);
+        documentSession.Delete<Product>(command.Id);
 
-            await documentSession.SaveChangesAsync(cancellationToken);
+        await documentSession.SaveChangesAsync(cancellationToken);
 
-            return new DeleteProductCommandResult(true);
-        }
+        return new DeleteProductCommandResult(true);
     }
 }
