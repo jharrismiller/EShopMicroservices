8538133cec64fc1faadbc359676b537a33f0f782
jharrismiller <36432119+jharrismiller@users.noreply.github.com>
2025-03-07 15:46:49 -0500

    Fix Error handling in basket

--- src/Servcies/Basket/Basket.API/Program.cs
+++ src/Servcies/Basket/Basket.API/Program.cs
@@ -1,19 +1,42 @@
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using bbExp = BuildingBlocks.Exceptions;
+using bbBhav = BuildingBlocks.Behaviors;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 var thisAssembly = typeof(Program).Assembly;
 
+var cnBasketDb = builder.Configuration.GetConnectionString("BasketDb")!;
+
 builder.Services.AddCarter();
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(thisAssembly);
-    //config.AddOpenBehavior(typeof(bb.Behaviors.ValidationBehavior<,>));
-    //config.AddOpenBehavior(typeof(bb.Behaviors.LoggingBehavior<,>));
+    config.AddOpenBehavior(typeof(bbBhav.ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(bbBhav.LoggingBehavior<,>));
 });
+
+builder.Services.AddMarten(config =>
+{
+    config.Connection(cnBasketDb);
+}).UseLightweightSessions();
+
+builder.Services.AddValidatorsFromAssembly(thisAssembly);
+builder.Services.AddExceptionHandler<bbExp.Handler.CustomExceptionHandler>();
+builder.Services.AddProblemDetails();
+//builder.Services.AddHealthChecks();
+
 
 var app = builder.Build();
 
 app.MapCarter();
+
+app.UseExceptionHandler();
+//app.UseHealthChecks("/health",
+//    new HealthCheckOptions
+//    {
+//        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+//    });
+
 
 app.Run();
