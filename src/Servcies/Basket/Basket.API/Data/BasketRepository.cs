19a1f3b07a9411e9a8d92cead9fafb09cfe90428
jharrismiller <36432119+jharrismiller@users.noreply.github.com>
2025-03-07 15:58:19 -0500

    Basket Resposity

---
+++ src/Servcies/Basket/Basket.API/Data/BasketRepository.cs
@@ -0,0 +1,28 @@
+﻿namespace Basket.API.Data;
+
+public class BasketRepository(IDocumentSession documentSession) : IBasketRepository
+{
+    public async Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
+    {
+        var shoppingCart = await documentSession.LoadAsync<ShoppingCart>(userName, cancellationToken);
+
+        if (shoppingCart is null)
+            throw new BasketNotFoundException(userName);
+
+        return shoppingCart;
+    }
+
+    public async Task<ShoppingCart> StoreBasket(ShoppingCart shoppingCart, CancellationToken cancellationToken = default)
+    {
+        documentSession.Store(shoppingCart);
+        await documentSession.SaveChangesAsync(cancellationToken);
+        return shoppingCart;
+    }
+
+    public async Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
+    {
+        documentSession.Delete<ShoppingCart>(userName);
+        await documentSession.SaveChangesAsync(cancellationToken);
+        return true;
+    }
+}
